cbdc50e77125716db11b43fc234de2f6eaabc827
malikmuhammadumair <numl-s24-13671@numls.edu.pk>
2026-02-23 01:31:26 +0500

    Update UserService.java

--- src/main/java/cofee/example/cofee/Services/UserService.java
+++ src/main/java/cofee/example/cofee/Services/UserService.java
@@ -23,14 +23,14 @@ public class UserService {
 
     public UserEntity PostnewAll(UserEntity userEntity) {
         userEntity.setPassword(passwordencorder.encode(userEntity.getPassword()));
-        userEntity.setRoles(Arrays.asList("ADMIN"));
+        userEntity.setRoles(Arrays.asList("USER"));
         return userRepository.save(userEntity);
     }
 
     //    for admin Creation g
     public UserEntity Addadmin(UserEntity userEntity) {
         userEntity.setPassword(passwordencorder.encode(userEntity.getPassword()));
-        userEntity.setRoles(Arrays.asList("USER"));
+        userEntity.setRoles(Arrays.asList("ADMIN"));
         return userRepository.save(userEntity);
     }
 
@@ -74,6 +74,7 @@ public class UserService {
 
 
 }
+
 
 
 
